fb80d525e4c202ffbd28bd03f6cb1df1af53aa59
Stefano De Pascalis <stefanodp91@gmail.com>
2018-03-02 16:36:20 +0100

    designed loading

--- src/pages/notifications-history/notifications-history.ts
+++ src/pages/notifications-history/notifications-history.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, LoadingController, Loading } from 'ionic-angular';
 
 // providers
 import { NotificationHistoryProvider } from '../../providers/notification-history';
 import { InAppBrowser } from '@ionic-native/in-app-browser';
+import { TranslateService } from '@ngx-translate/core';
+
+// context
+import { MyApp } from '../../app/app.component';
 
 @Component({
   selector: 'page-notifications-history',
@@ -17,12 +21,21 @@ export class NotificationsHistoryPage {
   // private DEFAULT_LIMIT : number = 50;  // default and max of onesignal
   // private DEFAULT_OFFSET : number = 0; // default of onesignal
 
+  private spinner : Loading;
+
   constructor(
-    public navCtrl: NavController,
-    public navParams: NavParams,
-    public notificationHistoryProvider: NotificationHistoryProvider,
-    public iab: InAppBrowser,
-  ) {
+    private navCtrl: NavController,
+    private navParams: NavParams,
+    private notificationHistoryProvider: NotificationHistoryProvider,
+    private iab: InAppBrowser,
+    private loadingCtrl: LoadingController,
+    private translate: TranslateService,) {
+      // ########### begin translations ###########
+      // this language will be used as a fallback when a translation isn't found in the current language
+      translate.setDefaultLang(MyApp.appConfig.defaultLanguage);
+      translate.use(MyApp.appConfig.defaultLanguage)
+      // ########### end translations ###########
+      this.spinner = this.createSpinner();
   }
 
   ionViewDidLoad() {
@@ -37,6 +50,8 @@ export class NotificationsHistoryPage {
   // private loadNotifications(refresher, limit, offset) {
   //  this.notificationHistoryProvider.load(limit, offset)
   private loadNotifications(refresher) {
+    this.showLoading();
+
     this.notificationHistoryProvider.load()
       .then(notifications => {
         // console.log("notifications", notifications);
@@ -44,6 +59,8 @@ export class NotificationsHistoryPage {
         // console.log("notifications", this.notifications);
 
         if(refresher !== undefined && refresher !== null) refresher.complete();
+
+        this.dismisLoading();
         
       });
   }
@@ -57,4 +74,26 @@ export class NotificationsHistoryPage {
     this.loadNotifications(refresher);
   }
 
+  private createSpinner() {
+    var message = this.translate.get('page_notifications_history_loading_message')['value'];
+
+     var spinner = this.loadingCtrl.create({
+      content: message,
+      // duration: duration,
+      dismissOnPageChange: true
+    });
+    return spinner;
+  }
+
+  private showLoading() {
+    if(this.spinner) {
+      this.spinner.present();
+    }
+  }
+
+  private dismisLoading() {
+    if(this.spinner) {
+      this.spinner.dismiss;
+    }
+  }
 }
